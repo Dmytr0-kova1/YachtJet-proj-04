53ae720ec1c0450c72e88cdfb20e40a5ca88d081
Dmytr0-kova1 <kdmytro793@gmail.com>
2024-08-09 14:17:24 +0000

    Deploying to gh-pages from @ Dmytr0-kova1/YachtJet-proj-04@2e50a8109331fc62f109c14d5042c5d8b0a7e56d 🚀

--- assets/mobile-menu-51cc45f6.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-menu-open]"),closeModalBtn:document.querySelector("[data-menu-close]"),modal:document.querySelector("[data-menu]")};e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o);function o(){e.modal.classList.toggle("is-open")}})();
-//# sourceMappingURL=mobile-menu-51cc45f6.js.map
